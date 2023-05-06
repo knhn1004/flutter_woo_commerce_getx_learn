7f7e4149c1a3b55f8ca06458cb88e0019c480b1c
knhn1004 <kunhan945@gmail.com>
2023-05-06 15:29:55 -0700

    load google fonts

--- lib/common/style/theme.dart
+++ lib/common/style/theme.dart
@@ -1,16 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import 'index.dart';
 
 /// 主题
 class AppTheme {
+  static final ThemeData _base = ThemeData(
+    textTheme: GoogleFonts.comfortaaTextTheme(),
+  );
+
   /// 亮色
-  static ThemeData light = ThemeData(
+  static ThemeData light = _base.copyWith(
     colorScheme: lightColorScheme,
   );
 
   /// 暗色
-  static ThemeData dark = ThemeData(
+  static ThemeData dark = _base.copyWith(
     colorScheme: darkColorScheme,
   );
 }
