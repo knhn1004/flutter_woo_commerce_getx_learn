5222bc6d7c92af30ccd4a64df1e98916424ca888
knhn1004 <kunhan945@gmail.com>
2023-05-13 12:27:29 -0700

    native splash and cleanup

--- lib/global.dart
+++ lib/global.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:get/get.dart';
 
 import 'common/index.dart';
 
 class Global {
   static Future<void> init() async {
-    WidgetsFlutterBinding.ensureInitialized();
+    WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+
+    FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+    // 删除设备启动图
 
     await Storage().init();
 
